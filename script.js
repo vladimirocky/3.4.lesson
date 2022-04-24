04daecf8a1266a4e5c423e00c4129c6bbfc6e5e1
Vidadi920 <vidadi886@gmail.com>
2022-04-25 02:19:24 +0300

    Complete

--- script.js
+++ script.js
@@ -13,12 +13,12 @@
 
 // какой-то объект который будем получать и менять асинхронно
 const data = {
-    name: "Some name",
-    modifyStatus: false,
-    numVal: 0
-}
+  name: 'Some name',
+  modifyStatus: false,
+  numVal: 0,
+};
 
-console.log("Start working..");
+console.log('Start working..');
 console.log(data);
 
 /**
@@ -26,19 +26,20 @@ console.log(data);
  * 1. Получить данные
  * 2. Изменить данные
  */
-console.log("Start processing data..");
+console.log('Start processing data..');
 
 setTimeout(() => {
-    /**
-     * Ваш код тут
-     * 1. изменить name
-     * 2. переключить флаг modifyStatus в true
-     * 3. вывести data
-     */
-    console.log("End processing data!")
+  //  * Ваш код тут
+  //  * 1. изменить name
+  data.name = 'One name';
+  //  * 2. переключить флаг modifyStatus в true
+  data.modifyStatus = true;
+  //  * 3. вывести data
+  console.log(data);
+  console.log('End processing data!');
 }, 2000);
 
-console.log("Continue work main program..");
+console.log('Continue work main program..');
 
 /**
  *
@@ -46,37 +47,47 @@ console.log("Continue work main program..");
  * Модификация данных
  *
  */
-const promise = new Promise(function (resolve,reject) {
-    console.log('Request data...');
-    setTimeout(() => {
-        /**
-         * Ващ код тут
-         * 1. передать data в функцию resolve
-         */
-    },8000)
-})
-
-promise.then(respData => {
-        console.log('Response ...');
+const promise = new Promise(function (resolve, reject) {
+  console.log('Request data...');
+  setTimeout(() => {
     /**
-     * Ваш код тут
-     * 1. вывести данные из промис
-     * 2. передать данные дальше по цепочке
+     * Ващ код тут
+     * 1. передать data в функцию resolve
      */
-    }
-).then( newData => {
+    resolve(data);
+  }, 8000);
+});
+
+promise
+  .then((respData) => {
+    console.log('Response ...');
+
+    //  * Ваш код тут
+    //  * 1. вывести данные из промис
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        //  * 2. передать данные дальше по цепочке
+        resolve(respData);
+      }, 2000);
+    });
+  })
+  .then((newData) => {
     console.log('Start modify data...');
-    setTimeout( () => {
-        /**
-         * Ваш код тут
-         * 1. переключить modifyStatus в true объекта
-         * 2. Изменить еще како-нибудь свойство объекта
-         * 3. Вывести объект
-         */
-        console.log('Stop modify data');
-    },8000)
-}).catch(err => console.log("Error",err))
-    .finally( () => console.log("Finally!"))
+    setTimeout(() => {
+      //  * Ваш код тут
+      //  * 1. переключить modifyStatus в true объекта
+      newData.modifyStatus = true;
+      //  * 2. Изменить еще како-нибудь свойство объекта
+      newData.numVal = 1;
+      //  * 3. Вывести объект
+      console.log(newData);
+
+      console.log('Stop modify data');
+      // return newData;
+    }, 8000);
+  })
+  .catch((err) => console.log('Error', err))
+  .finally(() => console.log('Finally!'));
 
 /**
  * Чтобы продемонстрировать генератор
@@ -85,25 +96,35 @@ promise.then(respData => {
  */
 
 function* random(limit) {
-    /**
-     * Ваш код тут
-     *
-     * let randomOddNumber = () => {
-     *      const result = Math.floor(Math.random() * limit);
-     * 1.     return -
-     *      result если он делится на 2 без остатка
-     *      иначе результат вызова randomOddNumber
-     * }
-     * 2. В бесконечном цикле возвращать результат вызова randomOddNumber()
-     * через yield
-     */
+  //  * Ваш код тут
+  //  *
+  let randomOddNumber = () => {
+    const result = Math.floor(Math.random() * limit);
 
+    if (result % 2 === 0) {
+      return result;
+    } else {
+      randomOddNumber();
+    }
+
+    //  * 1.     return -
+    //  *      result если он делится на 2 без остатка
+    //  *      иначе результат вызова randomOddNumber
+  };
+  //  * 2. В бесконечном цикле возвращать результат вызова randomOddNumber()
+  //  * через yield
+  yield randomOddNumber();
+  yield randomOddNumber();
+  yield randomOddNumber();
+
+  while (true) {
+    randomOddNumber();
+  }
 }
-const randomizer = random(100)
-console.log(randomizer.next().value)
-console.log(randomizer.next().value)
-console.log(randomizer.next().value)
-
+const randomizer = random(100);
+console.log(randomizer.next().value);
+console.log(randomizer.next().value);
+console.log(randomizer.next().value);
 
 /**
  * Работа async/ await
@@ -113,20 +134,23 @@ console.log(randomizer.next().value)
  *
  */
 async function generateArray() {
-    const arr = [53,342,3,235,63,56,546,457]
-    /**
-     * Ваш код тут
-     * 1. await new Promise ...
-     * передать в промис функцию, принимающую resolve и вызывающую resolve спустя секунду
-     */
-    return arr //  результат не вернется пока не выполнится промис
+  const arr = [53, 342, 3, 235, 63, 56, 546, 457];
+
+  //  * Ваш код тут
+  //  * 1. await new Promise ...
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  //  * передать в промис функцию, принимающую resolve и вызывающую resolve спустя секунду
+
+  return arr; //  результат не вернется пока не выполнится промис
 }
+console.log(generateArray());
 
 function f() {
-    /**
-     * Ваш код тут
-     * 1. вызвать асинхронную функцию, и отобразить ее результат
-     */
+  //  * Ваш код тут
+  //  * 1. вызвать асинхронную функцию, и отобразить ее результат
+  generateArray().then((kata) => {
+    console.log(kata);
+  });
 }
 
 f();
