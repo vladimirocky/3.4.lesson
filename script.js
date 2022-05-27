370968a6102ab7df6dcc0e98620d688fbcdf695e
Radli1 <i6100009@gmail.com>
2022-05-27 18:05:21 +0400

    enter

--- script.js
+++ script.js
@@ -15,12 +15,17 @@
 const data = {
     name: "Some name",
     modifyStatus: false,
-    numVal: 0
+    numVal: 0,
 }
 
 console.log("Start working..");
 console.log(data);
-
+setTimeout(()=> {
+    console.log("Downloading data...")
+}, 1000);
+setTimeout(()=>{
+    console.log("Processing data...")
+},3000);
 /**
  * Тут попытка через setTimeout имитировать
  * 1. Получить данные
@@ -28,7 +33,16 @@ console.log(data);
  */
 console.log("Start processing data..");
 
+const data = { 
+    name: "Some name",
+    modifyStatus: false,
+    numVal: 0,
+}
 setTimeout(() => {
+    data.name = "New name";
+    data.modifyStatus = "true";
+    console.log(data)
+    
     /**
      * Ваш код тут
      * 1. изменить name
@@ -46,18 +60,36 @@ console.log("Continue work main program..");
  * Модификация данных
  *
  */
+const data = { 
+    /**
+    /приходится каждый раз описывать переменную.Это правильно?
+    глобальную через "window." если задать, как ее сделать const?
+    */
+    name: "Some name",
+    modifyStatus: false,
+    numVal: 0,
+}
 const promise = new Promise(function (resolve,reject) {
     console.log('Request data...');
     setTimeout(() => {
+        if (data != ""){
+            resolve (data)
+        } else {
+            reject ('No data')
+        }
         /**
          * Ващ код тут
          * 1. передать data в функцию resolve
          */
-    },8000)
+    },2000)
 })
 
 promise.then(respData => {
-        console.log('Response ...');
+        console.log('Response ...', respData);
+        })
+        then(respData => {
+        console.log('Pesponse1...', respData);
+        
     /**
      * Ваш код тут
      * 1. вывести данные из промис
@@ -65,8 +97,11 @@ promise.then(respData => {
      */
     }
 ).then( newData => {
-    console.log('Start modify data...');
+    console.log('Start modify data...', newData);
     setTimeout( () => {
+        data.modifyStatus = true;
+        data.name = "New name";
+        console.log(data);
         /**
          * Ваш код тут
          * 1. переключить modifyStatus в true объекта
@@ -78,6 +113,7 @@ promise.then(respData => {
 }).catch(err => console.log("Error",err))
     .finally( () => console.log("Finally!"))
 
+
 /**
  * Чтобы продемонстрировать генератор
  * Создать генератор который выдает последовательность из случайных четных чисел
@@ -85,6 +121,20 @@ promise.then(respData => {
  */
 
 function* random(limit) {
+    let randomOddNumber = () => {
+    const result = Math.floor(Math.random() * limit);
+    for (let result of limit) {
+        if (result % 2 === 0){
+            yield result
+        }        
+    }
+}
+} 
+for (let result of random(limit)){
+    console.log(result)
+}
+
+
     /**
      * Ваш код тут
      *
@@ -98,11 +148,23 @@ function* random(limit) {
      * через yield
      */
 
-}
+
 const randomizer = random(100)
 console.log(randomizer.next().value)
 console.log(randomizer.next().value)
 console.log(randomizer.next().value)
+
+async function Rand () {
+    Array.from({length: 10}, () => Math.floor(Math.random() * 10));
+    
+    function rand() {
+        for (let num of Array) {
+            let result = await Rand(Array);
+            result += 0;
+            console.log(result);
+        }
+    }
+}
 
 
 /**
@@ -114,6 +176,9 @@ console.log(randomizer.next().value)
  */
 async function generateArray() {
     const arr = [53,342,3,235,63,56,546,457]
+    await new Promise(
+        resolve => setTimeout (resolve, 1000)
+    )
     /**
      * Ваш код тут
      * 1. await new Promise ...
@@ -123,6 +188,11 @@ async function generateArray() {
 }
 
 function f() {
+    async function f1(){
+        let result = a + b;
+        console.log(result);
+    }
+
     /**
      * Ваш код тут
      * 1. вызвать асинхронную функцию, и отобразить ее результат
